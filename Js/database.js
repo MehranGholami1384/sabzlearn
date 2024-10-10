6fbabac4293d564f5e1c81ab8113194fbe3b9ac5
Mehran Gholami <mehrangholami666@outlook.com>
2024-10-10 19:27:45 +0330

    complete last-courses-section

---
+++ Js/database.js
@@ -0,0 +1,125 @@
+const lastCourses = [{
+        imgSrc: './Pictures/Course-thumbnail-Algorithm.webp',
+        title: 'آموزش الگوریتم و ساختمان داده به زبان ساده',
+        info: 'ساختمان داده و الگوریتم، یک مهارت ضروری برای ورود به دنیای برنامه نویسیه که دیدگاه شما رو به مسائل مختلف…',
+        teacer: 'رضا دولتی',
+        score: 5,
+        students: 497,
+        price: 2300000,
+        offPercent: 30
+    },
+    {
+        imgSrc: './Pictures/elec-1-768x432.webp',
+        title: 'آموزش پروژه محور Electron JS',
+        info: 'ساختمان داده و الگوریتم، یک مهارت ضروری برای ورود به دنیای برنامه نویسیه که دیدگاه شما رو به مسائل مختلف…',
+        teacer: 'مهرشاد براتی',
+        score: 5,
+        students: 4,
+        price: 2200000,
+        offPercent: 30
+    },
+    {
+        imgSrc: './Pictures/nodejs.webp',
+        title: 'آموزش حرفه ای NodeJS بدون پیش نیاز',
+        info: 'الان و امروز کمتر کسی هست که از بازار کار داغ نود جی اس با خبر نباشه و یکی از…',
+        teacer: 'محمدامین سعیدی راد',
+        score: 5,
+        students: 705,
+        price: 3300000,
+        offPercent: 30
+    },
+    {
+        imgSrc: './Pictures/nestjs.webp',
+        title: 'آموزش پروژه محور NestJS از صفر!',
+        info: 'NestJS یه فریم‌ورک توسعه سمت سرور وب با TypeScript برای ساخت برنامه‌های مبتنی بر Node.js هست. نست بر پایه الگوی…',
+        teacer: 'معین باغشیخی',
+        score: 5,
+        students: 564,
+        price: 1999000,
+        offPercent: 30
+    },
+    {
+        imgSrc: './Pictures/python-data.webp',
+        title: 'Data Visualization با پایتون',
+        info: 'ما تو دنیایی زندگی میکنیم که همه چیز به سمت سریع تر شدن حرکت میکنه. برای ذهن انسان درک اشکال…',
+        teacer: 'رضا دولتی',
+        score: 5,
+        students: 383,
+        price: 619500,
+        offPercent: 30
+    },
+    {
+        imgSrc: './Pictures/api-php.webp',
+        title: 'آموزش جامع api نویسی با PHP',
+        info: 'API یکی از پر کاربردترین اجزای نرم افزاریه که حتما باید بلد باشی! توی این دوره قراره api نویسی رو…',
+        teacer: 'معین باغشیخی',
+        score: 5,
+        students: 101,
+        price: 1200000,
+        offPercent: 30
+    },
+    {
+        imgSrc: './Pictures/nextjs.webp',
+        title: 'آموزش Next.js بصورت پروژه محور',
+        info: 'نکست یه فریمورک مبتنی بر ری‌اکت هست که امروزه تو بازار کار یکی از مهم‌ترین تکنولوژی‌ها برای توسعه دهنده های…',
+        teacer: 'محمدامین سعیدی راد',
+        score: 5,
+        students: 1637,
+        price: 3500000,
+        offPercent: 30
+    },
+    {
+        imgSrc: './Pictures/gold-exp.webp',
+        title: 'تجربیات طلایی من در کار تیمی',
+        info: 'در ساده ترین حالت شما اگه تو یک شرکت برنامه نویسی استخدام بشین، وارد تیم اون شرکت میشین و باید…',
+        teacer: 'محمدامین سعیدی راد',
+        score: 5,
+        students: 341,
+        price: 375000,
+        offPercent: 30
+    },
+    {
+        imgSrc: './Pictures/zero-hacker.webp',
+        title: 'ورود به دنیای امنیت | هکر صفر کیلومتر',
+        info: 'اگر برای ورود به دنیای امنیت سردرگم هستید. این دوره با راهنمایی مختصر و مفید و ارائه موارد تجربی باعث…',
+        teacer: 'اشکان مقدس',
+        score: 5,
+        students: 21471,
+        price: 1500000,
+        offPercent: 100
+    },
+    {
+        imgSrc: './Pictures/Course-thumbnail-Dashboard2-1-768x432.webp',
+        title: 'پیاده سازی داشبورد های حرفه ای با CSS و JS',
+        info: 'این دوره فرصتی فوق‌العاده برای ارتقاء مهارت‌های فرانت اند شماست. با یادگیری از جدیدترین تکنیک‌ها، انیمیشن‌ها ، ابزارهای پیشرفته و…',
+        teacer: 'مهرشاد براتی',
+        score: 5,
+        students: 455,
+        price: 1400000,
+        offPercent: 30
+    },
+    {
+        imgSrc: './Pictures/Docker1-1-1-768x432.webp',
+        title: 'آموزش Docker از صفر مطلق!',
+        info: 'داکر یک پلتفرم متن باز برای طراحی؛ انتقال؛ استقرار و اجرای نرم افزار ها تحت مفهومی به نام Container می‌باشد،…',
+        teacer: 'آرمین هوشمند',
+        score: 5,
+        students: 141,
+        price: 1499000,
+        offPercent: 30
+    },
+    {
+        imgSrc: './Pictures/python-2.webp',
+        title: 'بهینه نویسی کد ها در پایتون',
+        info: 'همه میدونیم که زبان برنامه نویسی پایتون یه زبان خیلی جذاب، راحت و همینطور کاربردیه.  قبل از اینکه بخواین این…',
+        teacer: 'رضا دولتی',
+        score: 5,
+        students: 821,
+        price: 540000,
+        offPercent: 30
+    },
+]
+
+export {
+    lastCourses
+}
